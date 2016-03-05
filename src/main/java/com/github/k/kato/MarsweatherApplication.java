ea4bca8d924de00e6dbbe5cd35281df2a01801b5
k--kato <keisuke.k.kato@gmail.com>
2016-03-05 10:30:44 +0900

    initial commit

---
+++ src/main/java/com/github/k/kato/MarsweatherApplication.java
@@ -0,0 +1,12 @@
+package com.github.k.kato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsweatherApplication.class, args);
+	}
+}
