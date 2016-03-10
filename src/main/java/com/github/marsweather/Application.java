2a404d09d805b51c45f0397ab7dc232b3b5e406f
k--kato <keisuke.k.kato@gmail.com>
2016-03-10 22:07:55 +0900

    Mars Weather Jersey HTTP Client + Jackson JSON を add しました

--- src/main/java/com/github/marsweather/Application.java
+++ src/main/java/com/github/marsweather/Application.java
@@ -6,6 +6,7 @@
 package com.github.marsweather;
 
 import com.github.marsweather.domain.entity.DReport;
+import com.github.marsweather.domain.service.MaasApiService;
 import com.github.marsweather.domain.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,7 +14,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Date;
 import java.util.List;
 
 @SpringBootApplication
@@ -28,33 +28,28 @@ public class Application {
     @Autowired
     ReportService reportService;
 
-    // 当日データをデータベースに選択 & 削除 & 挿入(Select & Delete & Insert)する
+    @Autowired
+    MaasApiService maasApiService;
+
+    // 1か月分のデータを HTTP で取得し，データベースに選択 & 削除 & 挿入(Select & Delete & Insert)する
     @Bean
     CommandLineRunner runner() {
 
         return (args) -> {
 
-            // 当日日付を取得する
-            final Date today = new Date();
+            // 1か月分の火星の天気を取得する (Service -> Jersey HTTP Request MAAS API -> HTTP Response JSON -> Jackson -> Doma 2 Entity)
+            final List<DReport> reportsApi = maasApiService.getArchive("2016-02-01", "2016-02-29");
 
-            // Select を実行する (Doma 2 Entity -> Service -> Repository (DAO) -> PostgreSQL)
-            final List<DReport> reportToday = reportService.getReportsByTerrestrialDate(today);
+            reportsApi.forEach(reportApi -> {
+                // Select を実行する (Doma 2 Entity -> Service -> Repository (DAO) -> PostgreSQL)
+                final List<DReport> reportsDb = reportService.getReportsByTerrestrialDate(reportApi.terrestrialDate);
 
-            // Delete を実行する (Doma 2 Entity -> Service -> Repository (DAO) -> PostgreSQL)
-            if (reportToday != null) {
-                reportToday.forEach(report -> {
-                    if (report != null) {
-                        reportService.removeReport(report);
-                    }
-                });
-            }
-
-            // Entity を作成する
-            DReport reportTodayNew = new DReport();
-            reportTodayNew.terrestrialDate = today;
+                // Delete を実行する (Doma 2 Entity -> Service -> Repository (DAO) -> PostgreSQL)
+                reportsDb.forEach(reportDb -> reportService.removeReport(reportDb));
+            });
 
             // Insert を実行する (Doma 2 Entity -> Service -> Repository (DAO) -> PostgreSQL)
-            reportService.saveReport(reportTodayNew);
+            reportsApi.forEach(report -> reportService.saveReport(report));
 
         };
     }
